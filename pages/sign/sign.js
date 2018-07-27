c3ab0143602ed5d591224606e10bbaac4560bba1
e <ztshinemoi@163.com>
2018-07-27 17:41:06 +0800

    冲突解决

--- pages/sign/sign.js
+++ pages/sign/sign.js
@@ -1,66 +1,63 @@
 // pages/purchase/purchase.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-    imgPath:'/images/static/..',
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        init_step: 1,
+        step_condition: [1, 0, 0, 0, 0, 0],
+        prev_c:false,
+        next_c:true
+    },
+    imgPath: '/images/static/..',
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+    },
+    // prev: function(event) {
+    //     var step = event.currentTarget.dataset.stepid;
+    //     var state = [0, 0, 0, 0, 0, 0];
+    //     console.log(step)
+    //     state[step - 1] = 1;
+    //     this.setData({
+    //         step_condition: state
+    //     })
+    //     if (step == 1) {
+    //         this.setData({
+    //             prev_c: false
+    //         })
+    //     } else {
+    //         this.setData({
+    //             init_step: step - 1
+    //         })
+    //     }
+    //     console.log("hahahaha"+ data.prev_c)
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+    // },
+    next: function(event) {
+        var step = event.currentTarget.dataset.stepid;
+        var state = [0, 0, 0, 0, 0, 0];
+        state[step] = 1;
+        this.setData({
+            step_condition: state
+        })
+        this.setData({
+            init_step: step + 1
+        })
+        if(step+1>1){
+            console.log("hei")
+            this.data.prev_c = true;
+            console.log(this.data)
+        }
+        console.log(step);
+    }
 })
