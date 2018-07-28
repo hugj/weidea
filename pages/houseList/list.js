6335c700adac1020cda4eab74dfbf3003d61fdb5
hugj <huuugj@163.com>
2018-07-28 20:29:40 +0800

    搜索页下拉单样式

--- pages/houseList/list.js
+++ pages/houseList/list.js
@@ -1,6 +1,7 @@
 //list.js
 const util = require('../../utils/util.js');
 var app = getApp();
+var cityData = require('../../utils/city.js');
 
 Page({
     data: {
@@ -10,7 +11,24 @@ Page({
         name: "丽湖花园一期",
         type: "1室1厅",
         rent: 2300,
-      }]
+      }],
+
+      content: [],
+      nv: [],
+      px: [],
+      qyopen: false,
+      qyshow: false,
+      nzopen: false,
+      pxopen: false,
+      nzshow: false,
+      pxshow: false,
+      isfull: false,
+      cityleft: cityData.getCity(),
+      citycenter: {},
+      cityright: {},
+      select1: '',
+      select2: '',
+      shownavindex: ''
     },
 
     onLoad: function() {
@@ -35,7 +53,121 @@ Page({
           console.log(res)
         },
       })
+
+      this.setData({
+        nv: ['衣服', '裤子', '内衣', '服饰', '衣服', '裤子', '内衣', '服饰', '衣服', '裤子', '内衣', '服饰'],
+        px: ['默认排序', '离我最近', '价格最低', '价格最高']
+      })
     },
+  listqy: function (e) {
+    if (this.data.qyopen) {
+      this.setData({
+        qyopen: false,
+        nzopen: false,
+        pxopen: false,
+        nzshow: true,
+        pxshow: true,
+        qyshow: false,
+        isfull: false,
+        shownavindex: 0
+      })
+    } else {
+      this.setData({
+        qyopen: true,
+        pxopen: false,
+        nzopen: false,
+        nzshow: true,
+        pxshow: true,
+        qyshow: false,
+        isfull: true,
+        shownavindex: e.currentTarget.dataset.nav
+      })
+    }
+
+  },
+  list: function (e) {
+    if (this.data.nzopen) {
+      this.setData({
+        nzopen: false,
+        pxopen: false,
+        qyopen: false,
+        nzshow: false,
+        pxshow: true,
+        qyshow: true,
+        isfull: false,
+        shownavindex: 0
+      })
+    } else {
+      this.setData({
+        content: this.data.nv,
+        nzopen: true,
+        pxopen: false,
+        qyopen: false,
+        nzshow: false,
+        pxshow: true,
+        qyshow: true,
+        isfull: true,
+        shownavindex: e.currentTarget.dataset.nav
+      })
+    }
+  },
+  listpx: function (e) {
+    if (this.data.pxopen) {
+      this.setData({
+        nzopen: false,
+        pxopen: false,
+        qyopen: false,
+        nzshow: true,
+        pxshow: false,
+        qyshow: true,
+        isfull: false,
+        shownavindex: 0
+      })
+    } else {
+      this.setData({
+        content: this.data.px,
+        nzopen: false,
+        pxopen: true,
+        qyopen: false,
+        nzshow: true,
+        pxshow: false,
+        qyshow: true,
+        isfull: true,
+        shownavindex: e.currentTarget.dataset.nav
+      })
+    }
+    console.log(e.target)
+  },
+  selectleft: function (e) {
+
+    this.setData({
+      cityright: {},
+      citycenter: this.data.cityleft[e.currentTarget.dataset.city],
+      select1: e.target.dataset.city,
+      select2: ''
+    });
+  },
+  selectcenter: function (e) {
+
+    this.setData({
+      cityright: this.data.citycenter[e.currentTarget.dataset.city],
+      select2: e.target.dataset.city
+    });
+  },
+  hidebg: function (e) {
+
+    this.setData({
+      qyopen: false,
+      nzopen: false,
+      pxopen: false,
+      nzshow: true,
+      pxshow: true,
+      qyshow: true,
+      isfull: false,
+      shownavindex: 0
+    })
+  },
+
     onReady: function() {
       // console.log("list::onReady()");
     },
