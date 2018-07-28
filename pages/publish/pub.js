185a3bf353dfeab181490076250bad0666482b77
gdufeZLYL <18027894043@163.com>
2018-07-28 19:49:37 +0800

    放租房屋信息增加上传图片

--- pages/publish/pub.js
+++ pages/publish/pub.js
@@ -180,4 +180,26 @@ Page({
         housePrice: e.detail.value
       })
     },
+
+    //上传文件
+    upload: function () {
+      var that = this
+      wx.chooseImage({
+        success: function (res) {
+          var tempFilePaths = res.tempFilePaths
+          wx.uploadFile({
+            url: 'https://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
+            filePath: tempFilePaths[0],
+            name: 'file',
+            formData: {
+              'user': 'test'
+            },
+            success: function (res) {
+              var data = res.data
+              //do something
+            }
+          })
+        }
+      })
+    }
 })
