44d5ab504377c952b3162af6d6ccc6ded92b1961
e <ztshinemoi@163.com>
2018-07-28 17:05:11 +0800

    头顶绿更新

--- pages/publish/pub.js
+++ pages/publish/pub.js
@@ -6,9 +6,12 @@ Page({
      */
     data: {
         init_step: 1,
-        step_condition: [1, 0, 0, 0, 0, 0],
+        step_condition: [1, 0, 0, 0, 0],
         prev_c: false,
-        next_c: true
+        next_c: true,
+
+        /**房屋配置图片 */
+        conf_image:["bed","chest","bath","washing-machine","wifi"]
     },
     imgPath: '/images/static/..',
     /**
@@ -54,7 +57,7 @@ Page({
             // console.log("hei");
             this.setData({ prev_c: true });
         }
-        if (step + 1 == 6) {
+        if (step + 1 == 5) {
             this.setData({
                 next_c: false,
                 prev_c: false
@@ -69,7 +72,25 @@ Page({
 
     toSuccess: function () {
         wx.navigateTo({
-            url: '../rentered/complete'
+            url: '../landlord/landlord'
         })
+    },
+   
+    conf:function(event){
+        var id = event.currentTarget.dataset.id;
+        console.log(id);
+        var conf_img = this.data.conf_image;
+        var name = this.data.conf_image[id];
+        var new_name = name.indexOf('1') > 0 ? name.split('1')[0] :name + '1' ;
+        console.log('ori '+name);
+        console.log('new '+new_name);
+        // var change = this.data.conf_image[id] + "1";
+        conf_img[id] = new_name;
+       
+        this.setData({
+            conf_image:conf_img
+        })
+        console.log(this.data.conf_image)
+        
     }
 })
