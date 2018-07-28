9bb353a48f99f488c7794c809ee566ffcdc0756e
hugj <huuugj@163.com>
2018-07-28 22:02:45 +0800

    手写板

--- pages/sign/sign.js
+++ pages/sign/sign.js
@@ -3,6 +3,22 @@ const util = require('../../utils/util.js');
 var app = getApp();
 const contract = require('./contract.js')
 
+// canvas 全局配置
+var context = null;// 使用 wx.createContext 获取绘图上下文 context
+var isButtonDown = false;
+var arrx = [];
+var arry = [];
+var arrz = [];
+var canvasw = 0;
+var canvash = 0;
+//获取系统信息
+wx.getSystemInfo({
+  success: function (res) {
+    canvasw = res.windowWidth;//设备宽度
+    canvash = res.windowHeight;
+  }
+});
+
 Page({
 
     /**
@@ -31,8 +47,87 @@ Page({
           "sdate": "2018-07-01",
           "edate": "2018-08-01"
         },
+      src: ""
     },
     imgPath: '/images/static/..',
+
+    // 手写板
+  canvasIdErrorCallback: function (e) {
+    console.error(e.detail.errMsg)
+  },
+  canvasStart: function (event) {
+    isButtonDown = true;
+    arrz.push(0);
+    arrx.push(event.changedTouches[0].x);
+    arry.push(event.changedTouches[0].y);
+    //context.moveTo(event.changedTouches[0].x, event.changedTouches[0].y);
+  },
+  canvasMove: function (event) {
+    if (isButtonDown) {
+      arrz.push(1);
+      arrx.push(event.changedTouches[0].x);
+      arry.push(event.changedTouches[0].y);
+      // context.lineTo(event.changedTouches[0].x, event.changedTouches[0].y);
+      // context.stroke();
+      // context.draw()
+    };
+    for (var i = 0; i < arrx.length; i++) {
+      if (arrz[i] == 0) {
+        context.moveTo(arrx[i], arry[i])
+      } else {
+        context.lineTo(arrx[i], arry[i])
+      };
+    };
+    context.clearRect(0, 0, canvasw, canvash);
+    context.stroke();
+    context.draw(true);
+  },
+  canvasEnd: function (event) {
+    isButtonDown = false;
+  },
+  cleardraw: function () {
+    //清除画布
+    arrx = [];
+    arry = [];
+    arrz = [];
+    context.clearRect(0, 0, canvasw, canvash);
+    context.draw(true);
+  },
+  getimg: function () {
+    if (arrx.length == 0) {
+      wx.showModal({
+        title: '提示',
+        content: '签名内容不能为空！',
+        showCancel: false
+      });
+      return false;
+    };
+    //生成图片
+    wx.canvasToTempFilePath({
+      canvasId: 'canvas',
+      success: function (res) {
+        console.log(res.tempFilePath);
+        //存入服务器
+        wx.uploadFile({
+          url: 'a.php', //接口地址
+          filePath: res.tempFilePath,
+          name: 'file',
+          formData: { //HTTP 请求中其他额外的 form data 
+            'user': 'test'
+          },
+          success: function (res) {
+            console.log(res);
+          },
+          fail: function (res) {
+            console.log(res);
+          },
+          complete: function (res) {
+          }
+        });
+      }
+    })
+  },
+
     /**
      * 生命周期函数--监听页面加载
      */
@@ -62,6 +157,15 @@ Page({
       this.setData({
         tenantInfo: app.globalData.tenantInfo,
       });
+
+      //手写板
+      // 使用 wx.createContext 获取绘图上下文 context
+      context = wx.createCanvasContext('canvas');
+      context.beginPath()
+      context.setStrokeStyle('#ffff00');
+      context.setLineWidth(10);
+      context.setLineCap('round');
+      context.setLineJoin('round');
     },
     prev: function(event) {
         var step = event.currentTarget.dataset.stepid;
